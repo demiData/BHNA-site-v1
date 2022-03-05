fb64e1046c1bf19df87f3555dbf8f3d0f8419f31
demiData <demi.minjarez@gmail.com>
2022-03-05 14:14:29 -0800

    updated HomeTourCover

--- src/components/HomeTourCover/HomeTourCover.js
+++ src/components/HomeTourCover/HomeTourCover.js
@@ -19,15 +19,15 @@ const HomeTourCover = () => {
             
           
            <TourDetails className="about-text">
-            <h2>31st Bungalow Heaven Home Tour</h2>  
+            <h2>Encore 2022 Virtual Home Tour</h2>  
             <Seperator />
             <div>
                 <p className="tour__headline">As Seen Through A Lens</p>
-                <p className="tour__sub-headline">Historic Homes & Artisan Demonstrations</p>
+                <p className="tour__sub-headline">Streaming April 12-26, 2022</p>
             </div>
             <Seperator />
             <div>
-                <p className="tour__online">Thank you for your support, we hope you join us next year</p>
+                {/* <p className="tour__online">Thank you for your support, we hope you join us next year</p> */}
                 {/* <p className="tour__info"> <b>April 22-28, 2021</b> | Tickets now Available on Eventbrite</p> */}
                 {/* <a href="https://www.eventbrite.com/e/31st-bungalow-heaven-home-tour-as-seen-through-a-lens-tickets-142046383557" rel="noopener noreferrer" target="_blank">
                     <button>
